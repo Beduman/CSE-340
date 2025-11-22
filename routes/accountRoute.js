ee1c4190dac1b3ef2be00917b954d98b71a75790
Beduman <behunz77@gmail.com>
2025-11-22 14:29:14 -0700

    csss for inventory display

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,10 +1,11 @@
 // Needed Resources 
 const express = require("express")
 const router = new express.Router() 
+const utilities = require("../utilities/index")
 const accountController = require("../controllers/accountController")
 
 // Route to build login view
-router.get("/login", accountController.buildLogin);
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 // Process the login attempt
 router.post(
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -5,7 +5,7 @@ const utilities = require("../utilities/index")
 const invController = require("../controllers/invController")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
 //route to build inventory detail view
 router.get("/detail/:inv_id", utilities.handleErrors(invController.buildByInventoryId));
